dd6e6b4f69a6e60c1f28f85b7b7e3d6ff6f2c73c
Hasan Alp Doyduk <67971540+hasanalpdoyduk@users.noreply.github.com>
2023-02-13 13:47:00 +0300

    Create modules.py

---
+++ modules/modules.py
@@ -0,0 +1,14 @@
+#see the functions of the module
+dir(module)
+
+#obtain information about functions in module
+help(module)
+
+#using the imported module with a different name
+import math as matematik
+
+#using all module functions without typing the module name
+from math import *
+
+#using selected module functions without typing the module name
+from math import floor, ceil
